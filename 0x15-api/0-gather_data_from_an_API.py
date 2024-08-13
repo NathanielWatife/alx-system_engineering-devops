7e09dd7780eff088eca8f1ef65446854edaf168d
NathanielWatife <awosanminathniel0@gmail.com>
2024-08-13 16:41:46 +0100

    Worked on file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,26 +4,17 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(
+        url + "users/{}".format(sys.argv[1])
+    ).json()
+    todos = requests.get(
+        url + "todos", params={"userId": sys.argv[1]}
+    ).json()
 
-    user_id = sys.argv[2]
-    base_url = "https://jsonplaceholder.typicode.com/todos"
-    
-    try:
-        user_response = requests.get(f"{base_url}users/{user_id}")
-        user_response.raise_for_status()
-        user = user_response.json()
-        
-        todos_response = requests.get(f"{base_url}todos", params={"userId": user_id})
-        todos_response.raise_for_status()
-        todos = todos_response.json()
-        
-        completed = [t.get("title") for t in todos if t.get("completed") is True]
-        print("Employee {} is done with tasks({}/{}):".format(
-            user.get("name"), len(completed), len(todos)))
-        [print("\t {}".format(c)) for c in completed]
-
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
+    completed = [
+        t.get("title") for t in todos if t.get("completed") is True
+    ]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
