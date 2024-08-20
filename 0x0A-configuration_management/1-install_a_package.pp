041f8985cfdb05768a3014cc5be00df51862217f
NathanielWatife <awosanminathaniel0@gmail.com>
2024-08-20 05:14:37 -0700

    Added flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # install flask
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'Flask':
+	ensure   => '2.1.0',
+	provider => 'pip3',
+}
+
+package { 'Werkzeug':
+	ensure   => '2.0.3',
+  	provider => pip3,
 }
